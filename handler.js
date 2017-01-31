dffec3b517d4ad5d249232dd91f10153ce442e00
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-01-31 18:00:25 -0200

    Add createUser function

--- handler.js
+++ handler.js
@@ -1,16 +1,46 @@
-'use strict';
+'use strict'
 
-module.exports.hello = (event, context, callback) => {
+const hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      input: event
     }),
-  };
+  }
 
-  callback(null, response);
+  callback(null, response)
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+const buildResponse = (statusCode, data) => ({
+  statusCode,
+  body: JSON.stringify(data)
+})
+
+const createUser = (event, context, callback) => {
+  const body = JSON.parse(event.body)
+  const a1 = body.a1
+  const a2 = body.a2
+  const sum = a1 + a2
+
+  let response
+
+  if (!a1 || !a2) {
+    response = buildResponse(404, {
+      message: 'You need to pass `a1 and `a2`',
+      input: event
+    })
+  } else {
+    response = buildResponse(201, {
+      message: `User created with sum ${sum}`,
+      input: event
+    })
+  }
+
+  callback(null, response)
+}
+
+module.exports = {
+  hello,
+  createUser
+}
